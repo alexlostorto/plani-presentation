0dda3e46c18453c5fe200316f8fb3eccb818c9d2
Alex lo Storto <lostorto.business@gmail.com>
2023-09-27 18:26:38 +0100

    Make animations take up the entire viewport

--- components/animation.php
+++ components/animation.php
@@ -1,3 +1,3 @@
 <section class="d-flex justify-content-center align-items-center position-absolute h-100">
-    <video src="<?= $source ?>" width="1920" height="1080" autoplay loop muted playsinline" class="w-100 position-absolute"></video>
+    <video src="<?= $source ?>" width="1920" height="1080" autoplay loop muted playsinline" class="h-100 w-100 position-absolute"></video>
 </section>
